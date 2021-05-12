b1e4523f5d7ad37a063e2948ec0e8fadf60144c0
yanweipeng <csdd714342768@qq.com>
2021-05-12 18:04:01 +0800

    提交loin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,29 @@
 @extends('layouts.app')
 
+@section('css')
+    <style>
+        @media (min-width: 768px) {
+            .row{
+                margin-bottom: 20px;
+            }
+        }
+    </style>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-lg-5">
             <div class="card">
                 <div class="card-header">{{ __('登录') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('姓名') }}</label>
-
-                            <div class="col-md-6">
+                            <label for="email" class="col-md-2 col-lg-2 col-form-label text-md-right">{{ __('姓名') }}</label>
+                            <div class="col-md-9 col-lg-9">
                                 <input id="email" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -26,11 +33,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('密码') }}</label>
-
-                            <div class="col-md-6">
+                            <label for="password" class="col-md-2 col-lg-2 col-form-label text-md-right">{{ __('密码') }}</label>
+                            <div class="col-md-9 col-lg-9">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
